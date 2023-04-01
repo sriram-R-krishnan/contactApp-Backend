39f260237590120ff61994613e6dae0618a4e25a
sriram radhakrishnan <122258501+sriram-R-krishnan@users.noreply.github.com>
2023-04-01 18:05:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const authMiddleware = require("./middleware/auth")
 
 port = process.env.port
 
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 app.use(express.json());
 app.use(cors());
 app.use('/' , authRoute)
